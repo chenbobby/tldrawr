5301ca9d2212157b7aa10bb82f31e07c6c84836f
Bobby Chen <chenbobby168@gmail.com>
2017-07-25 12:00:45 -0700

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,57 @@
+/**
+ * Dependencies
+ */
+ import * as path from 'path';
+ import * as dotenv from 'dotenv';
+ import * as express from 'express';
+ import * as compression from 'compression';
+ import * as logger from 'morgan';
+ import * as bodyParser from 'body-parser';
+ import * as expressValidator from 'express-validator';
+ import * as errorHandler from 'errorhandler';
+ import * as passport from 'passport';
+
+ 
+ /**
+  * Load environment variables from .env file.
+  */
+dotenv.config({ path: '.env' });
+
+
+/**
+ * Create and configure Express server.
+ */
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+
+
+/**
+ * App Routes
+ */
+app.get('/', (req, res, next) => {
+    res.send('Hello World.');
+});
+
+
+/**
+ * Error Handler. Disabled for production.
+ */
+if (process.env.ENV === 'dev'){
+    app.use(errorHandler());
+}
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+    console.log(`Server running on localhost:${app.get('port')} ${app.get('env')}`);
+});
+
+module.exports = app;
